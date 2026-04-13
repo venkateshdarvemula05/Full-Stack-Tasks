1498ee96e14f74bada0939b1aa17b9eae9479714
Venkatesh Darvemula <venkateshdarvemula05@gmail.com>
2026-04-13 15:15:21 +0530

    Delete task10 directory

--- task10/src/main/java/com/example/demo/repository/Studentrepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.Student;
-public interface Studentrepository extends JpaRepository<Student,Integer>{
-
-}
