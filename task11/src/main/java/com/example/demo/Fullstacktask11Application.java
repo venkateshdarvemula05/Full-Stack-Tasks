c799ed4df729b67b07e6db785b7673397cbd51ee
Venkatesh Darvemula <venkateshdarvemula05@gmail.com>
2026-04-13 15:15:00 +0530

    Add files via upload

---
+++ task11/src/main/java/com/example/demo/Fullstacktask11Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fullstacktask11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fullstacktask11Application.class, args);
+	}
+
+}
