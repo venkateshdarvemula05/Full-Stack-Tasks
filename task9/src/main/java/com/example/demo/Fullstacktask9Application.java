e5da62a376e51092226623d4bc7300d60d161d6e
Venkatesh Darvemula <venkateshdarvemula05@gmail.com>
2026-04-13 15:11:26 +0530

    Add files via upload

---
+++ task9/src/main/java/com/example/demo/Fullstacktask9Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fullstacktask9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fullstacktask9Application.class, args);
+	}
+
+}
